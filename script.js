30e843218553e89bf253de929cb88c68d96b6e17
AaronSledge <ahern2118@gmail.com>
2024-01-04 00:48:26 -0800

    convert and display seconds to day hour minute remainder sec

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 
+
 function startBtn() {
     let date = document.getElementById("time").valueAsDate;
     let currentDate = new Date();
     let miliseconds = Math.abs(currentDate - date);
     let seconds = miliseconds / 1000;
     seconds = Math.round(seconds);
+    convert(seconds);
     document.getElementById("result").innerText = date.getUTCFullYear() + " " + currentDate.getFullYear() + " " + date.getUTCDay() + " " + miliseconds + " " + seconds;
+}
+
+function convert(seconds) {
+    let placeholder = seconds;
+    let days = Math.floor(placeholder / 86400);
+    placeholder = placeholder % 86400;
+
+    let hours = Math.floor(placeholder / 3600);
+    placeholder = placeholder % 3600;
+
+    let minutes = Math.floor(placeholder / 60);
+    placeholder = placeholder % 60;
+
+    let displaySeconds = placeholder;
+
+    display(days, hours, minutes, displaySeconds);
+}
+
+function display(days, hours, minutes, displaySeconds) {
+    document.getElementById("day").innerText = days;
+    document.getElementById("hour").innerText = hours + ":";
+    document.getElementById("minute").innerText = minutes + ":";
+    document.getElementById("second").innerText = displaySeconds;
 }
