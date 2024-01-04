a139df7763ee1c73f76247b7e7f95c9692e3293d
AaronSledge <ahern2118@gmail.com>
2024-01-04 10:50:57 -0800

    Time updates every second

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 
 function startBtn() {
-    let date = document.getElementById("time").valueAsDate;
-    let currentDate = new Date();
+    const date = document.getElementById("time").valueAsDate;
+    const currentDate = new Date();
     let miliseconds = Math.abs(currentDate - date);
     let seconds = miliseconds / 1000;
     seconds = Math.round(seconds);
     convert(seconds);
     document.getElementById("result").innerText = date.getUTCFullYear() + " " + currentDate.getFullYear() + " " + date.getUTCDay() + " " + miliseconds + " " + seconds;
+    let timer = setInterval(function() {
+        if (seconds >= 0) {
+            convert(seconds)
+            seconds--;
+        }
+        else {
+            clearInterval(timer);
+        }
+    }, 1000)
 }
 
 function convert(seconds) {
@@ -27,7 +36,7 @@ function convert(seconds) {
 }
 
 function display(days, hours, minutes, displaySeconds) {
-    document.getElementById("day").innerText = days;
+    document.getElementById("day").innerText = days + ":";
     document.getElementById("hour").innerText = hours + ":";
     document.getElementById("minute").innerText = minutes + ":";
     document.getElementById("second").innerText = displaySeconds;
