77561a0dd30cf967b82acb33e90595758cd32364
AaronSledge <ahern2118@gmail.com>
2024-01-04 00:17:15 -0800

    Convert difference in current date and input date in miliSecond

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
-
-function display() {
-    let date = document.getElementById("time").value;
-    document.getElementById("result").innerText = date;
+function startBtn() {
+    let date = document.getElementById("time").valueAsDate;
+    let currentDate = new Date();
+    let miliseconds = Math.abs(currentDate - date);
+    let seconds = miliseconds / 1000;
+    seconds = Math.round(seconds);
+    document.getElementById("result").innerText = date.getUTCFullYear() + " " + currentDate.getFullYear() + " " + date.getUTCDay() + " " + miliseconds + " " + seconds;
 }
